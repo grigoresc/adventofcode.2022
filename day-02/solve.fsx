a7c9fb9600ed9aae9591da14a133027b5e7b62c7
Ciprian Grigore <grigoresc@outlook.com>
2022-12-02 14:42:13 +0200

    day 2

--- day-02/solve.fsx
+++ day-02/solve.fsx
@@ -3,10 +3,7 @@ open System.IO
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 #load "../common.fsx"
-
 open Common
-
-//let convertToInt = List.map (List.map (fun (x: string) -> int x))
 
 let lines = File.ReadAllLines("input.txt")
 
@@ -14,32 +11,31 @@ let score (line: String) =
     let elf = line.[0]
     let me = line.[2]
 
-    let elfScore =
+    let elfIdx =
         match elf with
         | 'A' -> 1
         | 'B' -> 2
         | 'C' -> 3
-        | _ -> raise (Exception("elf"))
+        | _ -> failwith "elf"
 
-    let meScore =
+    let meIdx =
         match me with
         | 'X' -> 1
         | 'Y' -> 2
         | 'Z' -> 3
-        | _ -> raise (Exception("me"))
+        | _ -> failwith "me"
 
-    let score1 = meScore
+    let score1 = meIdx
 
     let score2 =
-        match (elfScore, meScore) with
+        match (elfIdx, meIdx) with
         | (1, 3) -> 0
         | (3, 1) -> 6
-        | (mes, elfs) when mes < elfs -> 6
-        | (mes, elfs) when mes = elfs -> 3
-        | (mes, elfs) when mes > elfs -> 0
-        | _ -> raise (Exception("game"))
+        | (elf, my) when elf < my -> 6
+        | (elf, my) when elf = my -> 3
+        | (elf, my) when elf > my -> 0
+        | _ -> failwith "score2"
 
-    //print $"{elf} vs {me} is {score1} and {score2}"
     score1 + score2
 
 let sln1 = lines |> Array.map score |> Array.sum //12156
@@ -48,35 +44,30 @@ print sln1
 let score2 (line: String) =
     let elf = line.[0]
     let res = line.[2]
-    //print me
-    //print elf
 
-    let elfScore =
+    let elfIdx =
         match elf with
         | 'A' -> 1
         | 'B' -> 2
         | 'C' -> 3
-        | _ -> raise (Exception("elf"))
+        | _ -> failwith "elf"
 
-    let resScore =
+    let score1 =
         match res with
         | 'X' -> 0
         | 'Y' -> 3
         | 'Z' -> 6
-        | _ -> raise (Exception("res"))
-
-    let score1 = resScore
+        | _ -> failwith "score1"
 
     let score2 =
-        match (elfScore, res) with
-        | (elfs, 'Y') -> elfs
+        match (elfIdx, res) with
+        | (elf, 'Y') -> elf
         | (1, 'X') -> 3
-        | (elfs, 'X') -> elfs - 1
+        | (elf, 'X') -> elf - 1
         | (3, 'Z') -> 1
-        | (elfs, 'Z') -> elfs + 1
-        | _ -> raise (Exception("game"))
+        | (elf, 'Z') -> elf + 1
+        | _ -> failwith "score2"
 
-    //print $"{elf} should {res} is {score1} and {score2}"
     score1 + score2
 
 let sln2 = lines |> Array.map score2 |> Array.sum //10835
