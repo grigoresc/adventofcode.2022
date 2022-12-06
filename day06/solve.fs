6155eb6a94b4b8fe58a0b16e762ca2dabc64797b
Ciprian Grigore <grigoresc@outlook.com>
2022-12-06 09:57:32 +0200

    day 6

--- day06/solve.fs
+++ day06/solve.fs
@@ -5,15 +5,14 @@ let ok (s: string) = Set(s).Count = s.Length
 let solve size (lines: string []) =
     let line = lines[0]
 
-    let sln =
+    let markers =
         seq {
             for i in [ 0 .. line.Length - size + 1 ] do
                 if ok (line[i + 0 .. i + size - 1]) then
                     yield (i + size)
         }
 
-    let x = sln |> Seq.item (0)
-    x
+    Seq.head markers
 
 let solve1 = solve 4
 let solve2 = solve 14
