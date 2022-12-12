ce7c610263649b09a4b334acc021884422708359
Ciprian Grigore <grigoresc@outlook.com>
2022-12-12 12:49:46 +0200

    day 12 1st part

---
+++ day12/test.fs
@@ -0,0 +1,33 @@
+module aoc.day12.test
+
+open aoc.day12.solutions
+open aoc.common
+
+open System.IO
+open Xunit
+
+let read x =
+    File.ReadAllLines(__SOURCE_DIRECTORY__ + "//" + x)
+
+let run solver filename = read filename |> solver
+
+[<Fact>]
+[<NCrunch.Framework.Timeout(600000)>]
+let ``Solve day 12 input`` () =
+    let res = run solve1 "input.txt"
+    Assert.True(504 = res)
+
+[<Fact>]
+let ``Solve day 12 sample`` () =
+    let res = run solve1 "sample.txt"
+    Assert.True(31 = res)
+
+[<Fact>]
+let ``Solve day 12 input part2`` () =
+    let res = run solve2 "input.txt"
+    Assert.True(0 = res)
+
+[<Fact>]
+let ``Solve day 12 sample part2`` () =
+    let res = run solve2 "sample.txt"
+    Assert.True(0 = res)
